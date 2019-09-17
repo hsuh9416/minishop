613c7809a70034b9cb04c81d3175371454d71843
haeun <suhhaeun87@gmail.com>
2019-09-17 15:05:25 +0900

    카카오맵 API 적용 완료

--- src/main/webapp/main/userContact.jsp
+++ src/main/webapp/main/userContact.jsp
@@ -10,10 +10,11 @@
 	<!--Map Container:START-->
 	<div class="row">
 		<div class="col">
-				<div id="staticMap" style="width:100%;height:350px;"></div>
+				<div id="kakaoMap" style="width:100%;height:350px;"></div>
 		</div>
 	</div>
 	<!--Map Container:END-->
+	
 	<!-- Contact Us:START -->
 	<div class="row">
 		<div class="col-lg-6 contact_col">
@@ -21,11 +22,11 @@
 				<h1>Contact Us</h1>
 				<p>There are many ways to contact us. You may drop us a line, give us a call or send an email, choose what suits you the most.</p>
 				<div>
-					<p>(800) 686-6688</p>
-					<p>info.kissingbug@gmail.com</p>
+					<p>(82) 010-3231-5839</p>
+					<p>kissingbug@gmail.com, suhhaeun87@gmail.com</p>
 				</div>
 				<div>
-					<p>26, Donhwamun-ro, Jongno-gu, Seoul, Republic of Korea</p>
+					<p>1-68, Sejong-daero, Jongno-gu, Seoul, Republic of Korea</p>
 				</div>
 				<div>
 					<p>Open hours: 9.00-18.00 Mon-Sat</p>
@@ -37,6 +38,6 @@
 	<!-- Contact Us:END -->	
 </div>
 	<!--JavaScript Additional LINK:START-->
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=e210ac65c606d46bf6ac7db34452f668"></script>
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=e210ac65c606d46bf6ac7db34452f668&libraries=services"></script>
 <script type="text/javascript" src="/minishop/resources/custom/js/general/map.kakao.js"></script>
 	<!--JavaScript Additional LINK:END-->
--- src/main/webapp/resources/custom/js/general/map.kakao.js
+++ src/main/webapp/resources/custom/js/general/map.kakao.js
@@ -2,21 +2,42 @@
  * 카카오맵 관련 javascript
  */
 /*API KAKAO 지도(도메인 등록 후에 사용 가능) */
-//이미지 지도에서 마커가 표시될 위치입니다 
-var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
 
-// 이미지 지도에 표시할 마커입니다
-// 이미지 지도에 표시할 마커는 Object 형태입니다
-var marker = {
-    position: markerPosition
-};
-
-var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지도를 표시할 div  
-    staticMapOption = { 
+//1. 맵 컨테이너 작성
+var container  = document.getElementById('kakaoMap'), // 이미지 지도를 표시할 div  
+    option = { 
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 이미지 지도의 중심좌표
         level: 3, // 이미지 지도의 확대 레벨
-        marker: marker // 이미지 지도에 표시할 마커 
     };    
+//2. 회사 위치 선택검색
+	// 이미지 지도를 생성합니다
+var map = new kakao.maps.Map(container, option);
 
-// 이미지 지도를 생성합니다
-var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+	//주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+	//주소로 좌표를 검색합니다
+geocoder.addressSearch('서울 종로구 세종로 1-68', function(result, status) {
+
+	  // 정상적으로 검색이 완료됐으면 
+    if (status === kakao.maps.services.Status.OK) {
+
+       var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+       // 결과값으로 받은 위치를 마커로 표시합니다
+       var marker = new kakao.maps.Marker({
+           map: map,
+           position: coords
+       });
+
+       // 인포윈도우로 장소에 대한 설명을 표시합니다
+       var infowindow = new kakao.maps.InfoWindow({
+           content: '<div style="width:150px;text-align:center;padding:6px 0;">KISSIN\'BUGS</div>'
+       });
+       infowindow.open(map, marker);
+
+       // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+       map.setCenter(coords);
+   } 
+});    
+
